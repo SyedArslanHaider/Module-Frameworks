a7b725831084ce6e3bd353bc6d411ca1926d3234
Syed Arslan <arslanhaider231@gmail.com>
2025-02-05 13:38:29 +0100

    level 4 complete

--- high-score-tables/src/components/App/App.jsx
+++ high-score-tables/src/components/App/App.jsx
@@ -1,17 +1,27 @@
 import HighScoreTable from "@/components/HighScoreTable/HighScoreTable.jsx";
 import scores from "@/data/scores.json";
 import "./App.scss";
+import { useState } from "react";
 
-const App = () => (
+const App = () => {
+  const [isAscending, setIsAscending] = useState(false);
+
+  const toggleSortOrder = () => {
+    setIsAscending(!isAscending);
+  };
+   return(
   <div className="app">
     <header className="app__header">
       <h1 className="app__heading">High Scores</h1>
+        <button className="toggle-button" onClick={toggleSortOrder}>
+        Sort {isAscending ? "Descending" : "Ascending"}
+        </button>
     </header>
     {scores.sort((a,b)=> a.name.localeCompare(b.name))
     .map(({ name, scores }, index) => (
-      <HighScoreTable country={name} scores={scores.sort((a,b)=> b.s - a.s)} key={index} />
+      <HighScoreTable country={name} scores={scores.sort((a, b) => isAscending ? a.s - b.s : b.s - a.s)}  key={index} />
     ))}
   </div>
 );
-
+}
 export default App;
